fbbffbab7c02818f5e2402ebf5c98c1b277f941e
Yemi Morrison <119580041+yemimorrison@users.noreply.github.com>
2024-02-01 22:47:18 +0100

    Update client.js
    Added js code for profile view

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
 displayView = function(){
-  // the code required to display a view
+	if (localStorage.getItem("loggedinusers") == null) {
+		document.getElementById('content').innerHTML = document.getElementById('welcomeview').innerHTML;
+	}
+	else {
+		token = JSON.parse(localStorage.getItem("member")).data;
+		if (JSON.parse(localStorage.getItem("loggedinusers"))[token] != undefined) {
+			already_login();
+		}
+		else {
+			document.getElementById('content').innerHTML = document.getElementById('welcomeview').innerHTML;
+		}
+	}
 };
 
-window.onload = function(){
-  console.log(localStorage.getItem("loggedinusers"));
-  console.log(localStorage.getItem("users"));
-  //code that is executed as the page is loaded.
-  //You shall put your own custom code here.
-  //window.alert() is not allowed to be used in your implementation.
-  //window.alert("Hello TDDD97!");
+window.onload = function() {
+	displayView();
 };
 
 
@@ -64,4 +70,154 @@ function saveContact(formData){
       document.getElementById('message').style.color = 'red';
     }
     
+}
+
+
+var already_login = function() {
+  token = JSON.parse(localStorage.getItem("member")).data;
+	email_member = JSON.parse(localStorage.getItem("loggedinusers"))[token];
+	password_member = JSON.parse(localStorage.getItem("users"))[email_member].password;
+
+	member = localStorage.setItem("member", JSON.stringify(serverstub.signIn(email_member, password_member)));
+	answer = JSON.parse(localStorage.getItem("member"));
+
+	if (answer.success) {
+		document.getElementById('content').innerHTML = document.getElementById('logged_in').innerHTML;
+		document.getElementById("tabs-1").style.display = 'block';
+		user_data();
+		post_to_wall();
+	}
+	else {
+		document.getElementById('span_test_3').innerHTML = answer.message;
+	}
+}
+
+var display_tab = function(element)  {
+  var tab_panels = document.getElementsByClassName('tab_panel');
+  for (var i = 0; i < tab_panels.length; i++) {
+      tab_panels[i].style.display = 'none';
+  }
+
+  var tabContentIdToShow = element.id.replace(/(\d)/g, '-$1');
+  document.getElementById(tabContentIdToShow).style.display = 'block';
+}
+
+var changing_password = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+email = JSON.parse(localStorage.getItem("loggedinusers"))[token];
+old_PSW = document.getElementById("change_old_psw").value;
+new_PSW = document.getElementById("change_new_psw").value;
+
+if ( old_PSW != JSON.parse(localStorage.getItem("users"))[email].password ) {
+  document.getElementById('span_test_4').innerHTML ="Password does not match";
+}
+else if (new_PSW.length < 5) {
+  document.getElementById('span_test_4').innerHTML ='Passwords should be at least 5 characters!';
+
+  }
+else if (new_PSW != document.getElementById("repeat_new_psw").value) {
+  document.getElementById('span_test_4').innerHTML ='Passwords should be the same!';
+
+}
+else {
+  psw_data = localStorage.setItem("psw_data", JSON.stringify(serverstub.changePassword(token, old_PSW, new_PSW)));
+  psw_output = JSON.parse(localStorage.getItem("psw_data"));
+  document.getElementById('span_test_4').innerHTML =psw_output.message;
+
+}
+}
+
+
+var signOut = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+signout_data = localStorage.setItem("signout_data", JSON.stringify(serverstub.signOut(token)));
+signout_output = JSON.parse(localStorage.getItem("signout_data"));
+if (signout_output.success) {
+  document.getElementById('content').innerHTML = document.getElementById('welcomeview').innerHTML;
+}
+}
+
+var user_data = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+getuser_data = localStorage.setItem("getuser_data", JSON.stringify(serverstub.getUserDataByToken(token)));
+getuser_output = JSON.parse(localStorage.getItem("getuser_data"));
+document.getElementById("email_output").innerHTML = getuser_output.data.email;
+document.getElementById("name_output").innerHTML = getuser_output.data.firstname;
+document.getElementById("familyname_output").innerHTML = getuser_output.data.familyname;
+document.getElementById("gender_output").innerHTML = getuser_output.data.gender;
+document.getElementById("city_output").innerHTML = getuser_output.data.city;
+document.getElementById("country_output").innerHTML = getuser_output.data.country;
+}
+
+var user_data_2 = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+email = document.getElementById("search_member").value;
+
+getuser_data_other = localStorage.setItem("getuser_data_other", JSON.stringify(serverstub.getUserDataByEmail(token,email)));
+getuser_output_other = JSON.parse(localStorage.getItem("getuser_data_other"));
+
+document.getElementById("email_output_2").innerHTML = getuser_output_other.data.email;
+document.getElementById("name_output_2").innerHTML = getuser_output_other.data.firstname;
+document.getElementById("familyname_output_2").innerHTML = getuser_output_other.data.familyname;
+document.getElementById("gender_output_2").innerHTML = getuser_output_other.data.gender;
+document.getElementById("city_output_2").innerHTML = getuser_output_other.data.city;
+document.getElementById("country_output_2").innerHTML = getuser_output_other.data.country;
+
+post_to_wall_2();
+}
+
+var post_to_wall = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+email = JSON.parse(localStorage.getItem("loggedinusers"))[token];
+message = document.getElementById("wall_thoughts").value;
+
+wall_data =  localStorage.setItem("wall_data", JSON.stringify(serverstub.postMessage(token, message, email)));
+wall_output = JSON.parse(localStorage.getItem("wall_data"));
+
+read_wall();
+}
+
+var read_wall = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+
+user_messages = localStorage.setItem("user_messages", JSON.stringify(serverstub.getUserMessagesByToken(token)));
+user_output = JSON.parse(localStorage.getItem("user_messages"));
+
+var text = ""
+for (i = 0; i < user_output.data.length; i++) {
+  if (user_output.data[i].content != "") {
+    text += "<b>" + user_output.data[i].writer + "</b>" + ":" + user_output.data[i].content + "<br><br>";
+  }
+}
+
+document.getElementById("theTextarea").innerHTML = text;
+}
+
+var post_to_wall_2 = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+email = document.getElementById("search_member").value;
+message = document.getElementById("wall_thoughts_2").value;
+
+
+wall_data =  localStorage.setItem("wall_data", JSON.stringify(serverstub.postMessage(token, message, email)));
+wall_output = JSON.parse(localStorage.getItem("wall_data"));
+
+read_wall_2();
+}
+
+var read_wall_2 = function() {
+token = JSON.parse(localStorage.getItem("member")).data;
+email = document.getElementById("search_member").value;
+
+user_messages = localStorage.setItem("user_messages", JSON.stringify(serverstub.getUserMessagesByEmail(token, email)));
+user_output = JSON.parse(localStorage.getItem("user_messages"));
+
+var text = ""
+for (i = 0; i < user_output.data.length; i++) {
+  if (user_output.data[i].content != "") {
+    text += "<b>" + user_output.data[i].writer + "</b>" + ":" + user_output.data[i].content + "<br><br>";
+  }
+}
+
+document.getElementById("theTextarea_2").innerHTML = text;
 }
